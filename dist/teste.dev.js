a29575d9d2155241128b8f4d9557cfc680e554ee
Francisco-Douglas <francisco_dougllas@outlook.com>
2024-11-11 14:58:12 -0300

    ----

---
+++ dist/teste.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var var1 = 10;
+var var2 = 20;
+var soma = var1 + var2;
+var subtracao = var1 - var2;
+var multiplicacao = var1 * var2;
+var divisao = var1 / var2;
+console.log('O resultado da soma é: ' + soma);
+console.log(soma, subtracao, multiplicacao, divisao);
---
+++ teste.js
@@ -0,0 +1,10 @@
+const var1 = 10;
+const var2 = 20;
+
+let soma = var1 + var2;
+let subtracao = var1 - var2;
+let multiplicacao = var1 * var2;
+let divisao = var1 / var2;
+
+console.log('O resultado da soma é: ' + soma);
+console.log(soma, subtracao, multiplicacao, divisao);
